24159166729c7990083bd9953d6b3a9cfef5d943
Roman Torres <rtorres@envivent.com>
2020-03-03 12:38:32 -0800

    updated to iframe and added content

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 class MediaPlayer {
     constructor(el){
+        // const that = this;
         this.el = document.querySelector(el);
+
+
     }
 
-    
+    nextVideo(url) {
+
+    }
+
+    changeVideo(url) {
+
+    }
+
+    hideThumb(){
+
+    }
+
 }
 
 let mp = new MediaPlayer(".mediacontainer");
