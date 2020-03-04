d1e8fbd63b29846c2800d3c6c48654cb4bef19b5
Roman Torres <rtorres@envivent.com>
2020-03-04 14:05:51 -0800

    updated to utilize template literals

--- js/main.js
+++ js/main.js
@@ -1,36 +1,75 @@
 class MediaPlayer {
     constructor(el){
-        // const that = this;
         this.el = document.querySelector(el);
-        this.allVids = this.el.querySelectorAll('li');
-        this.currentVideo = this.el.querySelector('video');
-        this.currentTitle = this.el.querySelector('.current_title');
-        this.currentDescription = this.el.querySelector('.current_description');
-        this.current = 0;
+        ///***Fetching json */
+        let videoContent = "js/vidcontent.json";
         
-        //***Initial Video Settings*/
-        this.allVids[this.current].classList.add("current");
-        this.currentVideo.src = this.allVids[this.current].querySelector('img').dataset['url'];
-        this.currentTitle.innerHTML = this.allVids[this.current].querySelector('h3').innerHTML;
-        this.currentDescription.innerHTML = this.allVids[this.current].querySelector('p').innerHTML;
 
-        ///***Autoplay */
-        this.currentVideo.addEventListener("canplay", () => {
-            this.currentVideo.play();
-        });
+        fetch(videoContent)
+            .then(response => response.json())
+            .then(videoContentData => {
+                this.data = videoContentData;
+                ///***Inserts JSON info into html */
+                this.buildMarkup();
 
-        ///***Auto Play Next */
-        this.currentVideo.addEventListener("ended", () => {
-            this.nextVideo();
-        });
+                ///***Variables */
+                this.allVids = this.el.querySelectorAll('li');
+                this.currentVideo = this.el.querySelector('video');
+                this.currentTitle = this.el.querySelector('.current_title');
+                this.currentDescription = this.el.querySelector('.current_description');
+                this.current = 0;
 
-        ///***Select Video from Playlist and scroll to top*/
+                
+                let title = this.data.vidcontent[0].title;
+                let description = this.data.vidcontent[0].description;
+                ///***Initial video settings */
+                this.allVids[this.current].classList.add("current");
+                this.currentVideo.src = this.data.vidcontent[0].url;
+                this.currentTitle.innerHTML = `Video Title: ${title}`;
+                this.currentDescription.innerHTML = `<h4>Description:</h4><br>${description}`;
+
+                ///***Call to functions for choosing vid from playlist and autoplay */
+                this.setEventListener();
+                this.autoPlay();
+        })
+            .catch(err => console.log(err));
+    }
+
+    buildMarkup() {
+        let buffer = [];
+
+        for (let i = 0; i < this.data.vidcontent.length; i++) {
+            console.dir(this.data.vidcontent[i]);
+            let image = this.data.vidcontent[i].image;
+            let title = this.data.vidcontent[i].title;
+            let description = this.data.vidcontent[i].description;
+            buffer.push(    
+            `<li>
+                <img src="${image}"/>
+                <div>
+                    <h3>${title}</h3>
+                    <p>${description}</p>
+                </div>
+            </li>`)
+        }
+        this.el.querySelector('aside').innerHTML = '<ul>' + buffer.join('') + '</ul>';
+    }
+
+    setEventListener() {
+        /***Select Video from Playlist */
         for (let i = 0; i < this.allVids.length ; i++) {
             this.allVids[i].addEventListener("click", () =>{
                 this.goto(i);
                 window.scrollTo(top);
             });
         };
+    }
+
+    autoPlay() {
+        /***Auto Play Next */
+        this.currentVideo.addEventListener("ended", () => {
+            this.nextVideo();
+        });
     }
 
     nextVideo() {
@@ -56,9 +95,13 @@ class MediaPlayer {
    
         ///***Make active classes */    
         this.allVids[this.current].classList.add("current");
-        this.currentVideo.src = this.allVids[this.current].querySelector('img').dataset['url'];
-        this.currentTitle.innerHTML = this.allVids[this.current].querySelector('h3').innerHTML;
-        this.currentDescription.innerHTML = this.allVids[this.current].querySelector('p').innerHTML;
+        this.currentVideo.src = this.data.vidcontent[i].url;
+        this.currentTitle.innerHTML = this.data.vidcontent[i].title;
+        this.currentDescription.innerHTML = this.data.vidcontent[i].description;
+        ///***Autoplay Video */
+        this.currentVideo.addEventListener("canplay", () => {
+            this.currentVideo.play();
+        });
     }
 
 }
